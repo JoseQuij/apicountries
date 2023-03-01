7a007dcc2d82c975cf7815ae6c1b5cf0cb0a84dc
Jose Quijije <josequijije5@gmail.com>
2023-03-01 12:08:20 -0600

    vers2

--- main.js
+++ main.js
@@ -1,24 +1,30 @@
-const inputField = document.querySelector('.input');
-const buttonSub = document.querySelector('.buttoninp');
-const result = document.querySelector('.results');
+const input = document.querySelector('.input')
+const button = document.querySelector('.buttoninp')
+const resuts = document.querySelector('.results')
+
+const picture = document.getElementById('picture')
+const fileupload = document.getElementById('fileupload')
 
 
-buttonSub.addEventListener("click", () => {
-    fetch(`https://restcountries.com/v3.1/name/${inputField.value}`)
-    .then((res) => res.json()).then((data) => postCountries(data));
-    inputField.value = ""
-});
+button.addEventListener('click' , () => {
+    fetch(`https://restcountries.com/v3.1/name/${input.value}`)
+    .then((res) => res.json()).then((data) => handlefunct(data))
 
-const postCountries = (data) => {
-        data.forEach((country) => {
-            console.log(country)
-           const div = document.createElement('div');
-            div.innerHTML = `
-                <img src="${country.flags.svg}">
-                <div>
-                    <h2>${country.altSpellings[1]}</h2>
-                    <h3>Continent:${country.continents}</h3>
-                </div>`
-            result.appendChild(div);
+    const handlefunct = (data) =>{
+        data.forEach(element => {
+            const div = document.createElement('div')
+            div.innerHTML =`
+            <img src=${element.flags.svg}>
+            <div>
+                <h1>${element.altSpellings[1]}</h1>
+                <h2>${element.continents}</h2>
+            </div>`
+            resuts.appendChild(div)
         });
-    };
+    }
+})
+
+
+fileupload.onchange = () => {
+    picture.src = URL.createObjectURL(fileupload.files[0])
+}
